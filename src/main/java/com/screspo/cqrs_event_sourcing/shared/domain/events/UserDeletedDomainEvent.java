722b4b5814409e0adb45a99e2cb79f8e100aa873
screspo <cresponline@gmail.com>
2022-02-05 15:58:57 +0100

    Refactor to CQRS: User deleted event

---
+++ src/main/java/com/screspo/cqrs_event_sourcing/shared/domain/events/UserDeletedDomainEvent.java
@@ -0,0 +1,67 @@
+package com.screspo.cqrs_event_sourcing.shared.domain.events;
+
+import com.screspo.cqrs_event_sourcing.shared.domain.bus.event.DomainEvent;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Objects;
+
+public final class UserDeletedDomainEvent extends DomainEvent {
+
+    public UserDeletedDomainEvent() {
+        super(null);
+    }
+
+    public UserDeletedDomainEvent(String aggregateId) {
+        super(aggregateId);
+    }
+
+    public UserDeletedDomainEvent(
+            String aggregateId,
+            String eventId,
+            String occurredOn
+    ) {
+        super(aggregateId, eventId, occurredOn);
+    }
+
+    @Override
+    public String eventName() {
+        return "user.deleted";
+    }
+
+    @Override
+    public HashMap<String, Serializable> toPrimitives() {
+        return new HashMap<>() {};
+    }
+
+    @Override
+    public UserDeletedDomainEvent fromPrimitives(
+            String aggregateId,
+            HashMap<String, Serializable> body,
+            String eventId,
+            String occurredOn
+    ) {
+        return new UserDeletedDomainEvent(
+                aggregateId,
+                eventId,
+                occurredOn
+        );
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        UserDeletedDomainEvent that = (UserDeletedDomainEvent) o;
+        return Objects.equals(aggregateId(), that.aggregateId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(aggregateId());
+    }
+}
--- src/main/java/com/screspo/cqrs_event_sourcing/users/application/use_cases/delete/UserRemover.java
+++ src/main/java/com/screspo/cqrs_event_sourcing/users/application/use_cases/delete/UserRemover.java
@@ -1,18 +1,25 @@
 package com.screspo.cqrs_event_sourcing.users.application.use_cases.delete;
 
+import com.screspo.cqrs_event_sourcing.shared.domain.bus.event.EventBus;
+import com.screspo.cqrs_event_sourcing.shared.domain.events.UserDeletedDomainEvent;
 import com.screspo.cqrs_event_sourcing.users.domain.UsersRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
 
 @Service
 public class UserRemover {
 
+    private final EventBus eventBus;
     private final UsersRepository usersRepository;
 
-    public UserRemover(UsersRepository usersRepository) {
+    public UserRemover(UsersRepository usersRepository, EventBus eventBus) {
         this.usersRepository = usersRepository;
+        this.eventBus = eventBus;
     }
 
     public void remove(String id) {
         usersRepository.delete(id);
+        eventBus.publish(Collections.singletonList(new UserDeletedDomainEvent(id)));
     }
 }
