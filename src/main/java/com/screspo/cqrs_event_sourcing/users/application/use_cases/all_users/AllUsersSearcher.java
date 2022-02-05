51cf6bb9696b4c0fab704adf68064102fd369f44
screspo <cresponline@gmail.com>
2022-02-05 19:19:45 +0100

    Refactor to CQRS: remove help dev event fetcher

--- src/main/java/com/screspo/cqrs_event_sourcing/users/application/use_cases/all_users/AllUsersSearcher.java
+++ src/main/java/com/screspo/cqrs_event_sourcing/users/application/use_cases/all_users/AllUsersSearcher.java
@@ -1,6 +1,5 @@
 package com.screspo.cqrs_event_sourcing.users.application.use_cases.all_users;
 
-import com.screspo.cqrs_event_sourcing.shared.domain.events.EventStoreRepository;
 import com.screspo.cqrs_event_sourcing.users.application.dtos.UserDTO;
 import com.screspo.cqrs_event_sourcing.users.application.dtos.UsersDTO;
 import com.screspo.cqrs_event_sourcing.users.domain.UsersRepository;
@@ -13,15 +12,12 @@ public class AllUsersSearcher {
 
     private final UsersRepository userRepository;
 
-    private final EventStoreRepository eventStoreRepository;
-
-    public AllUsersSearcher(UsersRepository usersRepository, EventStoreRepository eventStoreRepository) {
+    public AllUsersSearcher(UsersRepository usersRepository) {
         this.userRepository = usersRepository;
-        this.eventStoreRepository = eventStoreRepository;
+
     }
 
     public UsersDTO search() {
-        System.out.println(eventStoreRepository.fetch());
         return new UsersDTO(userRepository.searchAll()
                 .stream()
                 .map(UserDTO::fromAggregate)
